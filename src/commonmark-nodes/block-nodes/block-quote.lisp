2791a5cc5d75511d4a506946e3018d2696de3495
LF <yana-m@posteo.net>
2025-01-09 09:13:42 -0800

    move nodes around

--- src/commonmark-nodes/block-nodes/block-quote.lisp
+++
@@ -1,6 +0,0 @@
-
-(in-package #:clmark)
-
-(defblock (block-quote "^\\s{0,3}>\\s?" "^\\s{0,3}>\\s?" "^\\s$" :dc t)
-          (block-node parent-node child-node)
-          ())
--- src/commonmark-nodes/block-nodes/thematic-break.lisp
+++
@@ -1,9 +0,0 @@
-
-(in-package :clmark)
-
-(defblock (thematic-break
-           "^\\s{0,3}(-{3,}|_{3,}|\\*{3,})\\s*$"
-           +null-regex+
-           +null-regex+)
-          (leaf-block-node child-node)
-          ())
